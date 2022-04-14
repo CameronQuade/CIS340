e6712ef068996cd4c6895b3c490e866b12445bb4
CameronQuade <103219564+CameronQuade@users.noreply.github.com>
2022-04-14 20:57:48 +0000

    My Second Project Week 13

---
+++ MySecondProject/App copy.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+export default function MyApp() {
+  const pet = "Dog";
+  return (
+      <Text>
+        {"\n\n\n\n\n"}
+        Hello, I am a CIS340 student! {"\n"}
+        have a {pet}!
+        </Text>
+   
+  );
+}
